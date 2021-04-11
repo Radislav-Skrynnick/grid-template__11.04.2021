57449093b54671426d2823a3940991fca8e1d768
Radislav Skrynnick <mc.blackbord666@gmail.com>
2021-04-11 17:56:39 +0300

    init

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+
+  $('.header-navbar__btn').on('click', function () {
+    $(this).toggleClass('active');
+    $('.header-navbar__list').toggleClass('active');
+  });
+
+});
+
+// $(document).ready(function(){
+// 	$('.header-navbar__btn').on('click', function(){
+//     $(this).toggleClass('active');
+//     $('.header-navbar__list').stop(true, true).slideToggle(500);
+// 	});
+// });
